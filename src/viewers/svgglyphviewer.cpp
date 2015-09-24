8080b2caf66de554baffa90fb5352ece8b00232a
ampext <cowsayhelloworld@gmail.com>
2015-09-24 22:52:16 +0300

    enabled windows theme support for MSVS compiler

--- src/viewers/svgglyphviewer.cpp
+++ src/viewers/svgglyphviewer.cpp
@@ -2,6 +2,11 @@
 
 #include <wx/app.h>
 
+#ifdef _MSC_VER
+	#pragma comment(linker,"\"/manifestdependency:type='win32' \
+	name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+	processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
 
 class SvgGlyphViewer: public wxApp
 {
