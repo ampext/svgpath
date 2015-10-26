99d6c6e1d370f7be103cd03d76d8f2522917f064
ampext <cowsayhelloworld@gmail.com>
2015-10-26 21:36:56 +0300

    glyph preview dialog improvements

--- src/viewers/glyphpreviewdialog.cpp
+++ src/viewers/glyphpreviewdialog.cpp
@@ -1,7 +1,10 @@
 #include "glyphpreviewdialog.h"
 
 #include <svgglyphctrl.h>
+
 #include <wx/sizer.h>
+#include <wx/stattext.h>
+#include <wx/textctrl.h>
 
 #include <array>
 
@@ -16,7 +19,21 @@ GlyphPreviewDialog::GlyphPreviewDialog(wxWindow *parent, const SvgGlyph &glyph):
 		hSizer->Add(new SvgGlyphCtrl(this, wxID_ANY, glyph, 64), 0, wxALIGN_LEFT | wxALIGN_BOTTOM, 0);
 		hSizer->Add(new SvgGlyphCtrl(this, wxID_ANY, glyph, 16), 0, wxALIGN_LEFT | wxALIGN_BOTTOM, 0);
 
+		wxFlexGridSizer *gridSize = new wxFlexGridSizer(2, 5, 5);
+
+		gridSize->AddGrowableCol(1, 1);
+
+		gridSize->Add(new wxStaticText(this, wxID_ANY, L"Name"), 0, wxALIGN_CENTER_VERTICAL, 0);
+		gridSize->Add(new wxTextCtrl(this, wxID_ANY, glyph.glyphName, wxDefaultPosition, wxDefaultSize, wxTE_READONLY), 0, wxALIGN_CENTER_VERTICAL, 0);
+
+		wxString unicode;
+		unicode.Printf("%04x", glyph.unicode[0]);
+
+		gridSize->Add(new wxStaticText(this, wxID_ANY, L"Unicode"), 0, wxALIGN_CENTER_VERTICAL, 0);
+		gridSize->Add(new wxTextCtrl(this, wxID_ANY, unicode, wxDefaultPosition, wxDefaultSize, wxTE_READONLY), 0, wxALIGN_CENTER_VERTICAL, 0);
+
 		sizer->Add(hSizer, 1, wxALL | wxEXPAND, 5);
+		sizer->Add(gridSize, 0, wxALL | wxEXPAND, 5);
 	}
 
 	sizer->Add(CreateButtonSizer(wxOK), 0, wxALL | wxEXPAND, 5);
